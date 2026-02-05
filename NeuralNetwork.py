f8be351344ff709ecda2172c7207b75de7c1f236
N. Connor Whitaker <Connornwhit@gmail.com>
2026-02-05 13:04:59 -0500

    Delete NeuralNetwork.py

--- NeuralNetwork.py
+++
@@ -1,7 +0,0 @@
-from keras import layers, models, Sequential, losses, optimizers
-import numpy as np
-import pandas as pd
-
-# Read in csv datasets ---> dataframes
-df_test_motion = pd.read_csv("test_motion_data.csv")
-df_train_motion = pd.read_csv("train_motion_data.csv")
